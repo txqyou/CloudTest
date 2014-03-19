0afca931adc61e43ad19af8ff75f7b57505df757
uestctxq <txqyou@gmail.com>
2014-03-19 10:39:25 +0800

    add third

--- src/main/java/org/sonatype/mavenbook/web/SimpleServlet.java
+++ src/main/java/org/sonatype/mavenbook/web/SimpleServlet.java
@@ -8,6 +8,7 @@ public class SimpleServlet extends HttpServlet{
          public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException{
                 PrintWriter out = response.getWriter();
                 out.println("SimpleServlet Executed");
+		out.println("test again ");
                 out.flush();
                 out.close();
         }
